35226e359ad4b59bdb1d3f517d56261bcf479f07
Kyle Barron <kyle@developmentseed.org>
2024-11-26 20:11:47 -0500

    Add benches w/ copy to geo geometry & encoding (#37)
    * Add bench with copy to geo geometry
    
    * Add write benches

--- benches/parse.rs
+++ benches/parse.rs
@@ -1,6 +1,7 @@
 use std::str::FromStr;
 
 use criterion::{criterion_group, criterion_main};
+use geo_traits::to_geo::ToGeoGeometry;
 use wkt::Wkt;
 
 fn load_small_wkt() -> Wkt<f64> {
@@ -34,6 +35,34 @@ fn bench_parse(c: &mut criterion::Criterion) {
     c.bench_function("parse big", |bencher| {
         bencher.iter(|| {
             let _ = wkb::reader::read_wkb(&big_wkb).unwrap();
+        });
+    });
+
+    c.bench_function("parse small to geo", |bencher| {
+        bencher.iter(|| {
+            let wkb_geom = wkb::reader::read_wkb(&small_wkb).unwrap();
+            let _geo_types_geom = wkb_geom.to_geometry();
+        });
+    });
+
+    c.bench_function("parse big to geo", |bencher| {
+        bencher.iter(|| {
+            let wkb_geom = wkb::reader::read_wkb(&big_wkb).unwrap();
+            let _geo_types_geom = wkb_geom.to_geometry();
+        });
+    });
+
+    c.bench_function("encode small", |bencher| {
+        bencher.iter(|| {
+            let mut buf = Vec::new();
+            wkb::writer::write_geometry(&mut buf, &small, Default::default()).unwrap();
+        });
+    });
+
+    c.bench_function("encode big", |bencher| {
+        bencher.iter(|| {
+            let mut buf = Vec::new();
+            wkb::writer::write_geometry(&mut buf, &big, Default::default()).unwrap();
         });
     });
 }
